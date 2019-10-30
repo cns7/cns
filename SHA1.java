2730f2e944e10a319ea12f12befc0b74e720f888
cns7 <57140272+cns7@users.noreply.github.com>
2019-10-30 11:19:32 +0530

    Update SHA1.java

--- SHA1.java
+++ SHA1.java
@@ -1,19 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sha1;
 import java.nio.*;
+import java.util.Scanner;
 
 /*
 *   Created by Rosetta
 *   Recreated by Logesh B, Logesh D
 *
-*   The below program works only for input length < 32
+*   The below program works only for input length < 56
 */
 
-public class SHA1 {
+public class Sha1 {
     private static int INIT_A = 0x67452301;
     private static int INIT_B = 0xEFCDAB89;
     private static int INIT_C = 0x98BADCFE;
@@ -21,21 +16,27 @@ public class SHA1 {
     private static int INIT_E = 0xC3D2E1F0;
 
     public static void main(String[] args) {
-        String plainText = "";
+        Scanner sc = new Scanner(System.in);
+        String plainText = sc.nextLine();
+        //String plainText = "geeksforgeeks";
+        
         int len = plainText.length();
         
-        //Convert into 64-bit format & fill data for 63rd & len-th position
+        //Convert into 64-byte format & fill data for len-th & 56th byte
         byte[] TotalMessage = new byte[64];
         for(int i=0; i < len; i++)
             TotalMessage[i] = (byte)plainText.charAt(i);
         
         TotalMessage[len] = (byte)0x80; //10000000
-        TotalMessage[63] = (byte)(len * 8);
         
-        //Convert 64-bit data to 80-bit data
+        byte[] temp = ByteBuffer.allocate(8).putLong(len * 8).array();
+        for(int i=0; i<8; i++)
+            TotalMessage[56 + i] = temp[i];
+        
+        //Convert 64-byte data to 80-byte data
         int X[] = new int[80];
         for(int i=0, k=0; i < 16; i++) {
-            byte[] temp = new byte[4];
+            temp = new byte[4];
             
             for(int j=0; j < 4; j++)
                 temp[j] = TotalMessage[k++];
@@ -53,8 +54,8 @@ public class SHA1 {
         int d = INIT_D;
         int e = INIT_E;
         
-        for(int j = 0; j < 80; j++) {
-            int div20 = j/20;
+        for(int i = 0; i < 80; i++) {
+            int div20 = i/20;
             int f = 0;
             int k = 0;
             
@@ -78,26 +79,26 @@ public class SHA1 {
             }
           
             //Interchange values
-            int temp = Integer.rotateLeft(a, 5) + f + e + k + X[j];
+            int tmp = Integer.rotateLeft(a, 5) + f + e + k + X[i];
             e = d;
             d = c;
             c = Integer.rotateLeft(b, 30);
             b = a;
-            a = temp;
+            a = tmp;
         }
         
-        INIT_A += a;
-        INIT_B += b;
-        INIT_C += c;
-        INIT_D += d;
-        INIT_E += e;
+        a += INIT_A;
+        b += INIT_B;
+        c += INIT_C;
+        d += INIT_D;
+        e += INIT_E;
         
         byte[] encText = ByteBuffer.allocate(20)
-                    .putInt(INIT_A)
-                    .putInt(INIT_B)
-                    .putInt(INIT_C)
-                    .putInt(INIT_D)
-                    .putInt(INIT_E)
+                    .putInt(a)
+                    .putInt(b)
+                    .putInt(c)
+                    .putInt(d)
+                    .putInt(e)
                     .array();
 
         //Print the encrypted text in Hex format
